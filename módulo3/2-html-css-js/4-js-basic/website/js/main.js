ec6d3b694f1e1b2433b5ed53e3db120971037c21
emersonpessoa01 <eps011108@gmail.com>
2024-09-27 18:02:37 -0300

    feat: finished scrolltop :tada:

--- módulo3/2-html-css-js/4-js-basic/website/js/main.js
+++ módulo3/2-html-css-js/4-js-basic/website/js/main.js
@@ -169,26 +169,3 @@ links.forEach((link) => {
 // Insere todo o HTML acumulado de uma vez
 linkContainer.innerHTML = linksHTML;
 
-window.onload = function() {
-  let scrollTopButton = document.querySelector(".scrollTop");
-
-  // Se o botão existir na página
-  if (scrollTopButton) {
-    window.onscroll = function() {
-      if (window.scrollY > 100) {
-        scrollTopButton.classList.add("visible");
-        scrollTopButton.classList.remove("hidden");
-      } else {
-        scrollTopButton.classList.add("hidden");
-        scrollTopButton.classList.remove("visible");
-      }
-    };
-
-    scrollTopButton.onclick = function() {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-      });
-    };
-  }
-};
--- módulo3/2-html-css-js/4-js-basic/website/js/scrollTop.js
+++ módulo3/2-html-css-js/4-js-basic/website/js/scrollTop.js
@@ -1,24 +1,10 @@
-window.onload = function() {
-    let scrollTopButton = document.querySelector(".scrollTop");
-  
-    // Se o botão existir na página
-    if (scrollTopButton) {
-      window.onscroll = function() {
-        if (window.scrollY > 100) {
-          scrollTopButton.classList.add("visible");
-          scrollTopButton.classList.remove("hidden");
-        } else {
-          scrollTopButton.classList.add("hidden");
-          scrollTopButton.classList.remove("visible");
-        }
-      };
-  
-      scrollTopButton.onclick = function() {
-        window.scrollTo({
-          top: 0,
-          behavior: "smooth"
-        });
-      };
-    }
-  };
-  
+let scrollTop = document.querySelector(".scrollTop");
+window.onscroll = () => {
+  scrollTop.classList.toggle("active", window.scrollY > 0);
+};
+scrollTop.onclick = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
