afaafba21481c1fdaff80e95a0c8480868e7e350
emersonpessoa01 <eps011108@gmail.com>
2024-10-03 14:14:23 -0300

    fix Changed the content of data.json and image :sparkles:

--- módulo3/2-html-css-js/4-js-basic/landingpage-ecommerce/public/js/main.js
+++ módulo3/2-html-css-js/4-js-basic/landingpage-ecommerce/public/js/main.js
@@ -6,25 +6,21 @@ menu.onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// window.onscroll = ()=>{
-//     menu.classList.add(".fa-times");
-//     navbarNav.classList.remove("active");
-//     if(window.scrollY > 0 ){
-//         document.querySelector("#scroll-top").classList.add("active");
-//     }else{
-//         document.querySelector("#scroll-top").classList.remove("active");
-//     }
-// }
-
 const getJson = async () => {
   try {
-    let url = `https://jsonplaceholder.typicode.com/users/`;
+    let url = `https://landingpage-ecommerce-git-master-emersonpessoa01s-projects.vercel.app/api/data`;
     let response = await fetch(url);
     let json = await response.json();
+    console.log(json.produtos);
 
-    console.log(json);
+    // let home = renderHome(json.produtos);
+    // console.log(home);
+    // let containerHome = document.getElementById("container-home");
+    // containerHome.innerHTML = home;
   } catch (error) {
     console.log(`Error: ${error.message}`);
   }
 };
 getJson();
+
+
