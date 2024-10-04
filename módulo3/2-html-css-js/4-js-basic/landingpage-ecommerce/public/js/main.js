b4cb4a66e9ec99d2317fc31804fbc4d0a8e7b043
emersonpessoa01 <eps011108@gmail.com>
2024-10-04 18:03:35 -0300

    fix: finished home :tada:

--- módulo3/2-html-css-js/4-js-basic/landingpage-ecommerce/public/js/main.js
+++ módulo3/2-html-css-js/4-js-basic/landingpage-ecommerce/public/js/main.js
@@ -6,21 +6,50 @@ menu.onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
+// Função para buscar os dados da API
 const getJson = async () => {
   try {
-    let url = `https://landingpage-ecommerce-git-master-emersonpessoa01s-projects.vercel.app/api/data`;
+    let url = "/api/data";
     let response = await fetch(url);
-    let json = await response.json();
-    console.log(json.produtos);
-
-    // let home = renderHome(json.produtos);
-    // console.log(home);
-    // let containerHome = document.getElementById("container-home");
-    // containerHome.innerHTML = home;
+    let { produtos } = await response.json(); // Assume que os produtos estão no array 'produtos'
+    console.log(produtos);
+    // Chama a função para renderizar os produtos
+    // renderProducts(produtos);
   } catch (error) {
     console.log(`Error: ${error.message}`);
   }
 };
+
+// Executa a função de busca de dados
 getJson();
 
+// Função para renderizar os produtos
+// const renderProducts = (produtos) => {
+//   const swiperWrapper = document.querySelector(".swiper-wrapper");
+//   produtos.forEach((produto) => {
+//     const getSlide = `
+//       <div class="swiper-slide">
+//         <div class="bg-indigo-50 rounded-2xl h-96 flex justify-center items-center">
+//           <img src="${produto.special_image}" alt="Slide 1" class="w-full h-full object-cover rounded-2xl" />
+//         </div>
+//       </div>
+//     `;
+//     swiperWrapper.innerHTML += getSlide;
+//   });
+// };
 
+let swiper = new Swiper(".default-carousel", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 3000, // Tempo em milissegundos entre as transições
+    disableOnInteraction: false, // O autoplay não será desativado após a interação do usuário
+  },
+});
